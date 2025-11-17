21f38f4880208f267997c8b2e96a014867f409ea
Sriram Vema <74271047+sriramvema@users.noreply.github.com>
2025-11-17 00:07:46 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function Home() {
     formData.append("file", selectedFile);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/upload-pdf", {
+      const response = await fetch("/upload-pdf", {
         method: "POST",
         body: formData,
       });
